36ea113c904c6f8a76ce8d044c7d953cb1b43d5d
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-01-27 23:50:55 +0000

    “lists”

--- lists.py
+++ lists.py
@@ -1,6 +1,21 @@
 l = []
 l.append ("one")
 l.append ("two")
-l.append (str("three"))
+l.append ("three")
+l.append ('four')
+# to print the full list
+# print (l)
+# to print one specific element (list starts at 0)
+# print (l[1])
+# to remove one specific element (list starts at 0) pop also works
+# del (l[1])
+# print (l)
+# to clear the list but not delete it
+# l.clear()
+# print (l)
+# combining two lists
+l2 = ['five', 'six']
 print (l)
-print (str(l))
+print (l2)
+l.extend(l2)
+print (l)
