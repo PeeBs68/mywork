79fd3c311e128d33f6c0c2f37dbef8ba8ddddbaf
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-01-31 20:29:13 +0000

    inputs_and_outputs

---
+++ inputs.py
@@ -0,0 +1,9 @@
+# sample for taking in input and printing to the console
+
+age = input("What is your age? ")
+# print(age)
+print ("Your age is", age)
+
+name = input("What is your name? ")
+# print(name)
+print ("Your name is", name)
