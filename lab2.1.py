5b06015428dafeb1761afc57d3a2ffb44e00e44e
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-02-01 23:57:55 +0000

    Lab2.1 Updates with new operators

--- lab2.1.py
+++ lab2.1.py
@@ -5,6 +5,17 @@ print (21 * 55)
 print (21 / 4)
 # to only return an int answer (use the // floor division)
 print (21 // 4) 
+# To get the remainder (use the % operator)
+print (21%4)
+# powers
+print (2**8)
+# For comparisons 
+# 2 == 2 would result True
+# 2 == 4 would return False
 
-# 
+
+# Statements can span multiple lines like this 
+print (( 2 +
+4
++ 4))
 
