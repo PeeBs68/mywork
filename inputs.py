6ab875d9059c008eb2467b0cc2542c1f8556f5a6
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-02-01 21:53:04 +0000

    Added function f

--- inputs.py
+++ inputs.py
@@ -3,6 +3,7 @@
 # Adding int takes the input as an integer rather than string
 age = int(input("What is your age? "))
 # print(age)
+# Preferable way to print
 print (f"Your age is {age}")
 
 # print ("Your age is", age) this would also work but use the above format
