5642105f6481ef0bdf88409890f23a97569cdca6
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-02-02 19:56:39 +0000

    lab2.2

--- lab2.2.py
+++ lab2.2.py
@@ -8,4 +8,8 @@ print ("Hello", name)
 # \n for new line
 print ("Hello " + name + "\n\nHi there!\n")
 # OR
-print (f"Hello {name}\n\nHi there!\n")
+# print (f"Hello {name}\n\nHi there!\n")
+
+number = int(input("Enter a number: "))
+newnumber = number + 1
+print (f"{number} plus 1 is {newnumber}")
