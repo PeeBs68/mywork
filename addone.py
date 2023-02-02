9bc656a6cd6ec4a2bb61fcf594d9858afc27f7a0
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-02-02 19:58:51 +0000

    addonev1

---
+++ addone.py
@@ -0,0 +1,7 @@
+# lab2.2.py
+# author: Phelim Barry
+# small programs
+
+number = int(input("Enter a number: "))
+newnumber = (number + 1)/100
+print (f"{number} plus 1 divided by 100 is {newnumber}")
