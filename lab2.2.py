c28df4aaf77cf38e83c33ad504eae5ad0e422321
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-02-02 19:52:24 +0000

    lab2.2

--- lab2.2.py
+++ lab2.2.py
@@ -1,1 +1,11 @@
-# creating small programs
+# lab2.2.py
+# author: Phelim Barry
+# small programs
+
+# Input and print your name
+name = input("Enter your name ")
+print ("Hello", name)
+# \n for new line
+print ("Hello " + name + "\n\nHi there!\n")
+# OR
+print (f"Hello {name}\n\nHi there!\n")
