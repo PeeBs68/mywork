352ededd9b6acff1b1b96e3385de5b4224807afc
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-02-02 20:04:28 +0000

    nameandagev1

---
+++ nameandage.py
@@ -0,0 +1,12 @@
+# nameandage.py
+# author: Phelim Barry
+# small programs in lab2.2
+
+name = input("What is your name? ")
+age = int(input("What is your age? "))
+
+print (f"Hello {name}, your age is {age}")
+# OR \t for a tab
+print (f"Hello {name},\t your age is {age}")
+# OR \n for a new line
+print (f"Hello {name},\nyour age is {age}")
