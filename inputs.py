2b65d069a2ef4d4ab9fd592fa42e1aa33a5cf5ce
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-02-07 20:30:56 +0000

    enter comment here

--- inputs.py
+++ inputs.py
@@ -2,14 +2,17 @@
 
 # Adding int takes the input as an integer rather than string
 age = int(input("What is your age? "))
-
-age = age/100
+#age = age/100
 # print(age)
 # Preferable way to print
-print (f"Your age is {age}")
+ageoutput = "Your age is {} years"
+print(ageoutput.format(age))
 
 # print ("Your age is", age) this would also work but use the above format
 
-name = input("What is your name? ")
-# print(name)
-print ("Your name is", name)
+myname = input("What is your name? ")
+#age = age/100
+print(myname)
+# Preferable way to print
+#ageoutput = "Your age is {} years"
+#print(ageoutput.format(age))
