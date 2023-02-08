4412d02aa8fc746036a8b3642fb3ff1bf7080fc2
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-02-08 20:17:59 +0000

    temp version of accounts.py

--- accounts_temp.py
+++ accounts_temp.py
@@ -1,1 +1,6 @@
 # Make the input a string (not int/float)
+
+#Maybe use len for the 2nd part of the assignment to get the lenght of the string 
+#so we know how many characters to X out
+
+#Always use print(f...)
