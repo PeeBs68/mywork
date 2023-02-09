ba82b00fa39882e8628668e272b2ed99f79a4cf6
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-02-09 23:24:07 +0000

    testTypes.py complete

--- Lab03/testTypes.py
+++ Lab03/testTypes.py
@@ -1,2 +1,22 @@
 # testTypes.py
+# playing with variable types
 
+int1 = 12
+var1 = type(int1)
+print (f"int1 is of type ",(var1), "and value is",int1)
+
+float1 = 12.75
+var2 = type(float1)
+print (f"int1 is of type ",(var2), "and value is",float1)
+
+boolean1 = True
+var3 = type(boolean1)
+print (f"int1 is of type ",(var3), "and value is",boolean1)
+
+str1 = "Hello"
+var4 = type(str1)
+print (f"int1 is of type ",(var4), "and value is",str1)
+
+list1 = [1,2,3]
+var5 = type(list1)
+print (f"int1 is of type ",(var5), "and value is",list1)
