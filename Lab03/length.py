dcbde870bc3dd1452fe9b26eb9267554883f42d5
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-02-10 21:25:00 +0000

    lab3.1 length.py complete

---
+++ Lab03/length.py
@@ -0,0 +1,6 @@
+# Script to read in a string and print out it's length
+
+string1 = input("Enter a string: ")
+len_str = len(string1)
+#\t for a tab. \n for a new line
+print(f"Input String is \t{string1} which is \n\t\t{len_str} characters long")
