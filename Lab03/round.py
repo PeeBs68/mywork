a440b79df042bfc875c600dbcf0b2fb62dfa88d3
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-02-10 20:44:40 +0000

    lab3.1 round.py complete

---
+++ Lab03/round.py
@@ -0,0 +1,6 @@
+# Script to round a number
+# NOTE - round only rounds to the nearest even number!!!
+
+number = float(input("Input number "))
+rounded = round(number)
+print(f"{number} rounded is {rounded}")
