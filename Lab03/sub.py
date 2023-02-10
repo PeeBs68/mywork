f2f1d9097f33d81b5afa50062defd0314db5606d
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-02-10 12:23:51 +0000

    First attempt at sub.py

---
+++ Lab03/sub.py
@@ -0,0 +1,9 @@
+# Script to read in two numbers, and subtract one from the other
+
+number1 = int(input("Enter first number: "))
+number2 = int(input("Enter Second number: "))
+result = int(number1 - number2)
+print(result)
+print(f"{number1} - {number2} is {result}")
+
+
