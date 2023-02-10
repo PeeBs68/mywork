2f65b0b1c2a3d5909e4daa29759c9338d9c6037a
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-02-10 12:19:02 +0000

    Updated print statements to use f string

--- Lab03/testTypes.py
+++ Lab03/testTypes.py
@@ -22,9 +22,14 @@ var5 = type(list1)
 #print (f"int1 is of type ",(var5), "and value is",list1)
 
 
-print('variable {} is of type:{} and value: {}'.format('int', type(int1), int1))
+print('\nvariable {} is of type:{} and value: {}'.format('int', type(int1), int1))
 print('variable {} is of type:{} and value: {}'.format('float1', type(float1), float1))
 print('variable {} is of type:{} and value: {}'.format('boolean1', type(boolean1), boolean1))
 print('variable {} is of type:{} and value: {}'.format('str1', type(str1), str1))
 print('variable {} is of type:{} and value: {}'.format('list1', type(list1), list1))
-print(f"variable Int1 is of type {var1} and value: {int1}")
+# Or we could use the following to print using the f string
+print(f"\nvariable Int1 is of type {var1} and value: {int1}")
+print(f"variable float1 is of type {var2} and value: {float1}")
+print(f"variable boolean1 is of type {var3} and value: {boolean1}")
+print(f"variable str1 is of type {var4} and value: {str1}")
+print(f"variable list1 is of type {var5} and value: {list1}")
