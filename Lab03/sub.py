3a80997ac6db9d8565f5ba73a4e9f2d9e7d31db8
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-02-10 13:35:37 +0000

    Added different print output

--- Lab03/sub.py
+++ Lab03/sub.py
@@ -4,6 +4,6 @@ number1 = int(input("Enter first number: "))
 number2 = int(input("Enter Second number: "))
 result = int(number1 - number2)
 print(result)
-print(f"{number1} - {number2} is {result}")
+print(f"Number1: {number1} - Number2: {number2} is Result: {result}")
 
 
