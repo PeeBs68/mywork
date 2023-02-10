9dfe945b6fa4a407d8bfa3e07a148fc3052d500a
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-02-10 21:32:12 +0000

    lab3.1 normalise.py started

---
+++ Lab03/normalise.py
@@ -0,0 +1,10 @@
+# Script to read in a string - strips and leading or trailing spaces and convert it to lower case
+# Output the length of the input and output strings and indicate the spaces saved
+
+# remember to add spaces at the start/end of the input
+string1 = input("Enter a string: ")
+#xxx = normalised string and in lower case
+#yyy = number of spaces saved (len1 - len2)
+
+print(f"The Original string is : {string1} \nAnd the normalised string is {xxx}, \nWe saved {yyy} spaces")
+
