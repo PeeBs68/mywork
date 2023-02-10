2dc175acaa6cd3ef15b94ec07d64479b8f557e70
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-02-10 20:59:52 +0000

    lab3.1 absolute.py complete

---
+++ Lab03/absolute.py
@@ -0,0 +1,6 @@
+# Script to give the absolut value for a number
+# absolute could be described as the distance from zero (it's always a positive value)
+
+number = (float(input("Enter the number: ")))
+absolute_number = abs(number)
+print(f"original number is {number} and the absolute value is {absolute_number}")
