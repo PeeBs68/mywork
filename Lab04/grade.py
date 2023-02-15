849350c3374502e431ef8a923fdda61b82c3f8d7
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-02-15 20:12:21 +0000

    grade.py Completed

---
+++ Lab04/grade.py
@@ -0,0 +1,15 @@
+# Script to calculate grades
+
+value = int(input("Enter the grade "))
+if value > 100 or value <0:
+    print ("Invalid grade")
+elif value <40:
+    print ("You failed")
+elif value >=40 and value <50:
+    print ("You got a Pass")
+elif value >=50 and value <60:
+    print ("You got a Merit 2")
+elif value >=60 and value <70:
+    print ("You got a Merit 1")
+else:
+    print ("You got a Distinction")
