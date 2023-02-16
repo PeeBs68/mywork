a4809f1141a1fed715fade2fe1f0be55f4f8b22f
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-02-16 15:12:35 +0000

    evens.py complete

---
+++ Lab04/evens.py
@@ -0,0 +1,15 @@
+# Script to print al the even numbers between 2 and 100
+
+num = 2
+
+while (num<101):
+    print (num)
+    num = num + 2
+# Or use num += 2
+print ("All Done")
+
+#if (number % 2) == 0:
+#    evenodd = "Even"
+#else:
+#    evenodd = "Odd"
+#print (f"{number} is an {evenodd} number")
