1036be4e8ccec59da224af74f2c1e2b7e37b252a
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-02-22 20:59:35 +0000

    created temp.py for general testing of anything

---
+++ Lab04/temp.py
@@ -0,0 +1,10 @@
+# For general testing of anything...
+
+num = int(input("Enter a number "))
+while num != -1:
+    if (num %2 == 0):
+        print ("num is Even")
+    else:
+        print ("num is odd")
+    num = int(input("Enter a number "))
+print ("Finished")
