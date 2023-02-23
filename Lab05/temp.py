e9ceaafde137f435352d4c3b3562b2af7c391ec6
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-02-23 23:37:39 +0000

    Play file

---
+++ Lab05/temp.py
@@ -0,0 +1,19 @@
+# For general playing around
+
+# Andrews solution to lab 5.2 using pop
+import random
+queue = []
+numberOfNumbers=10
+rangeTo=100
+
+for n in range (0, numberOfNumbers):
+    queue.append(random.randint(0,rangeTo))
+    
+print (f"queue is {queue}")
+
+while len(queue) != 0:
+    currentNumber = queue.pop(0)
+    print (f"current num is {currentNumber} and the queue is {queue}")
+
+print ("the queue is now empty")
+
