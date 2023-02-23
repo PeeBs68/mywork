5b03f0a4d513a15131b49770a1c91e06f60b0002
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-02-23 21:35:44 +0000

    Lab5.2 completed

---
+++ Lab05/months.py
@@ -0,0 +1,23 @@
+# Create a tuple and storre months of the year 
+# Then crate another from that for just May, Jun, July
+
+months = ("January", "February", "March", "April", "May", "June", "July")
+
+summer_months = months[4:7]
+for month in summer_months:
+    print (month)
+
+
+
+
+#numbers = []
+#num = int(input("Please enter a number "))
+#while num != 0:
+#    numbers.append(num)
+#    num = int(input("Please enter another number "))
+#for num in numbers:
+#    print (num)
+# get the average - as of time of writing we haven't done this bit so copied from the solution
+# sum of the list divided by the lenght of the list
+#average = float(sum(numbers))/len(numbers)
+#print(f"The average is: {average}")
