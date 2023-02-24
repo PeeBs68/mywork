ec6cfad7b42402c273b4bb56ad09137ff2925a94
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-02-24 21:51:28 +0000

    Lab5.4 extra and still only partially working

--- Lab05/course_list_extra.py
+++ Lab05/course_list_extra.py
@@ -1,53 +1,27 @@
 # lab5.4 create a dict by reading in a student name, courses and grades
 # break when the module entered is blank
 
-student_file = {
-    "name" : "Mary", 
-    "Results" : [{
-        "Course" : "Programming",
-        "Grade" : 99 },
-        {"Course" : "Scripting",
-        "Grade" : 77
-        }
-    ]
-}
-student_file = {}
 student_file2 = {
-    "Name" : [],
-    "Results" : [{ 
-        "Module" : [], 
-        "Grade" : []
-        }
+    "Name" : "", 
+    "Results" : [{
+        "Course" : "",
+        "Grade" : ""}
     ]
 }
 
-print (student_file2)
-person1 = input("Enter a name: ")
-#student_file["Name"]=person1
-student_file2["Name"]=person1
-module1 = "temp" #dummy just to make the 1st loop run
-# Finish if nothing is entered for module
-while len(module1)!= 0:
-    module1 = input("Enter a module: ")
-    if len(module1)>0:
-#        student_file["Module"]=module1
-        student_file2["Results"].append(module1)
-        grade1 = input("Enter a grade: ")
-#        student_file["Grade"]=grade1
-        student_file2["Results"].append(grade1)
-#print (student_file)
 #print (student_file2)
+person1 = input("Enter a name: ")
+student_file2["Name"]=person1
+course1 = "temp" #dummy just to make the 1st loop run
+# Finish if nothing is entered for module
+while len(course1)!= 0:
+    course1 = input("Enter a module: ")
+    if len(course1)>0:
+        student_file2["Results"].append(course1)
+        grade1 = input("Enter a grade: ")
+        student_file2["Results"].append(grade1)
+print (student_file2)
 
-print(f"Name: {student_file2['Name']}")
-print(f"Results: {student_file2['Results']}")
-print(f"Grade: {student_file2['Grade']}")
-print(f"Results: {student_file2['Results']['Module']}")
-print(f"Results: {student_file2['Grade']}")
-#print (f"Module: {[student_file2.Module]['Results']}")
-
-#print (f"\t{student_file2['Module']} : {student_file2['Grade']}")
-#for x in student_file2["Results"]:
-#    print (f"Module :\t{student_file2['Results']} Grade : {student_file2['Results']}")
-
-#for course in student_file2["Module"]:
-#    print (f"\t{student_file2['Module']} : {student_file2['Grade']}")
+print(f"Student: {student_file2['Name']}")
+for course in student_file2["Results"]:
+    print (f"\t{course['Course']} : {course['Grade']}")
