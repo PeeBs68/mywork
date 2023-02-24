b0092b698c80368ad54759edd4fe2f6fec830024
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-02-24 12:42:27 +0000

    Lab5.4 Completed

--- Lab05/course_list.py
+++ Lab05/course_list.py
@@ -1,6 +1,15 @@
-# create a dict for 1 student name, courses and grades
+# lab5.4 create a dict for 1 student name, courses and grades
 
-student_file = {"name" : "Mary"},
-{"name" : "Mary"}
-# student_file = {"name" : "Mary", {Programming" : 45, History : 99}}
-print(student_file)
+student_file = {
+    "name" : "Mary", 
+    "Results" : [{
+        "Course" : "Programming",
+        "Grade" : 99 },
+        {"Course" : "Scripting",
+        "Grade" : 77
+        }
+    ]
+}
+print(f"Student: {student_file['name']}")
+for course in student_file["Results"]:
+    print (f"\t{course['Course']} : {course['Grade']}")
