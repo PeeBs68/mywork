df525eff680c889af607388a9f1dc2696e4eca85
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-02-24 15:54:39 +0000

    Lab5.4 extra partially working

--- Lab05/course_list_extra.py
+++ Lab05/course_list_extra.py
@@ -1,16 +1,47 @@
 # lab5.4 create a dict by reading in a student name, courses and grades
 # break when the module entered is blank
 
-student_file = {
-    "name" : "Mary", 
-    "Results" : [{
-        "Course" : "Programming",
-        "Grade" : 99 },
-        {"Course" : "Scripting",
-        "Grade" : 77
-        }
-    ]
-}
-print(f"Student: {student_file['name']}")
-for course in student_file["Results"]:
-    print (f"\t{course['Course']} : {course['Grade']}")
+student_file = {}
+
+person1 = input("Enter a name: ")
+student_file["Name"]=person1
+module1 = "temp" #dummy just to make the 1st loop run
+# Finish if nothing is entered for module
+while len(module1)!= 0:
+    module1 = input("Enter a module: ")
+    if len(module1)>0:
+        student_file["Module"]=module1
+        grade1 = input("Enter a grade: ")
+        student_file["Grade"]=grade1
+print (student_file)
+
+
+#while module1 != "":
+#    module1 = input("Enter a module: ")
+#    student_file["Module"]=module1
+#    grade1 = input("Enter a grade: ")
+#    student_file["Grade"]=grade1
+
+#print (student_file)
+
+#    "Name" : "", 
+#    "Results" : [{
+#        "Course" : "",
+#        "Grade" : 0}
+#    ]
+#}
+
+
+#student_file = {
+#    "name" : "Mary", 
+#    "Results" : [{
+#        "Course" : "Programming",
+#        "Grade" : 99 },
+#        {"Course" : "Scripting",
+#        "Grade" : 77
+#        }
+#    ]
+#}
+#print(f"Student: {student_file['name']}")
+#for course in student_file["Results"]:
+#    print (f"\t{course['Course']} : {course['Grade']}")
