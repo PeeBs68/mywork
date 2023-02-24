f90f7c0a5be4c6eb9a2ece8407fe1c1c7826f89b
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-02-24 01:20:27 +0000

    Play file

---
+++ Lab05/course_list.py
@@ -0,0 +1,6 @@
+# create a dict for 1 student name, courses and grades
+
+student_file = {"name" : "Mary"},
+{"name" : "Mary"}
+# student_file = {"name" : "Mary", {Programming" : 45, History : 99}}
+print(student_file)
