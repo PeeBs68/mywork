4fa463f15b7136ea680de319bffa0388735a2c89
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-02-24 16:25:21 +0000

    Lab5.4 extra still only partially working

--- Lab05/course_list_extra.py
+++ Lab05/course_list_extra.py
@@ -2,46 +2,31 @@
 # break when the module entered is blank
 
 student_file = {}
+student_file2 = {"Name" : [], "Module" : [], "Grade" : []}
 
 person1 = input("Enter a name: ")
-student_file["Name"]=person1
+#student_file["Name"]=person1
+student_file2["Name"]=person1
 module1 = "temp" #dummy just to make the 1st loop run
 # Finish if nothing is entered for module
 while len(module1)!= 0:
     module1 = input("Enter a module: ")
     if len(module1)>0:
-        student_file["Module"]=module1
+#        student_file["Module"]=module1
+        student_file2["Module"].append(module1)
         grade1 = input("Enter a grade: ")
-        student_file["Grade"]=grade1
-print (student_file)
-
-
-#while module1 != "":
-#    module1 = input("Enter a module: ")
-#    student_file["Module"]=module1
-#    grade1 = input("Enter a grade: ")
-#    student_file["Grade"]=grade1
-
+#        student_file["Grade"]=grade1
+        student_file2["Grade"].append(grade1)
 #print (student_file)
+#print (student_file2)
 
-#    "Name" : "", 
-#    "Results" : [{
-#        "Course" : "",
-#        "Grade" : 0}
-#    ]
-#}
+print(f"Name: {student_file2['Name']}")
 
+#print (f"Module: {student_file2['Module']}")
 
-#student_file = {
-#    "name" : "Mary", 
-#    "Results" : [{
-#        "Course" : "Programming",
-#        "Grade" : 99 },
-#        {"Course" : "Scripting",
-#        "Grade" : 77
-#        }
-#    ]
-#}
-#print(f"Student: {student_file['name']}")
-#for course in student_file["Results"]:
-#    print (f"\t{course['Course']} : {course['Grade']}")
+#print (f"\t{student_file2['Module']} : {student_file2['Grade']}")
+for x in student_file2["Module"]:
+    print (f"Module :\t{student_file2['Module']} Grade : {student_file2['Grade']}")
+
+for course in student_file2["Module"]:
+    print (f"\t{student_file2['Module']} : {student_file2['Grade']}")
