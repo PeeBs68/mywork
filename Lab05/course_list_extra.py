c7b10671fedd0d7b40b5ab27c6b5342bea0a5fd4
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-02-24 17:15:59 +0000

    Lab5.4 extra and still only partially working

--- Lab05/course_list_extra.py
+++ Lab05/course_list_extra.py
@@ -1,9 +1,27 @@
 # lab5.4 create a dict by reading in a student name, courses and grades
 # break when the module entered is blank
 
+student_file = {
+    "name" : "Mary", 
+    "Results" : [{
+        "Course" : "Programming",
+        "Grade" : 99 },
+        {"Course" : "Scripting",
+        "Grade" : 77
+        }
+    ]
+}
 student_file = {}
-student_file2 = {"Name" : [], "Module" : [], "Grade" : []}
+student_file2 = {
+    "Name" : [],
+    "Results" : [{ 
+        "Module" : [], 
+        "Grade" : []
+        }
+    ]
+}
 
+print (student_file2)
 person1 = input("Enter a name: ")
 #student_file["Name"]=person1
 student_file2["Name"]=person1
@@ -13,20 +31,23 @@ while len(module1)!= 0:
     module1 = input("Enter a module: ")
     if len(module1)>0:
 #        student_file["Module"]=module1
-        student_file2["Module"].append(module1)
+        student_file2["Results"].append(module1)
         grade1 = input("Enter a grade: ")
 #        student_file["Grade"]=grade1
-        student_file2["Grade"].append(grade1)
+        student_file2["Results"].append(grade1)
 #print (student_file)
 #print (student_file2)
 
 print(f"Name: {student_file2['Name']}")
-
-#print (f"Module: {student_file2['Module']}")
+print(f"Results: {student_file2['Results']}")
+print(f"Grade: {student_file2['Grade']}")
+print(f"Results: {student_file2['Results']['Module']}")
+print(f"Results: {student_file2['Grade']}")
+#print (f"Module: {[student_file2.Module]['Results']}")
 
 #print (f"\t{student_file2['Module']} : {student_file2['Grade']}")
-for x in student_file2["Module"]:
-    print (f"Module :\t{student_file2['Module']} Grade : {student_file2['Grade']}")
+#for x in student_file2["Results"]:
+#    print (f"Module :\t{student_file2['Results']} Grade : {student_file2['Results']}")
 
-for course in student_file2["Module"]:
-    print (f"\t{student_file2['Module']} : {student_file2['Grade']}")
+#for course in student_file2["Module"]:
+#    print (f"\t{student_file2['Module']} : {student_file2['Grade']}")
