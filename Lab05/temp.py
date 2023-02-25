f7b47945ba366f60268cfd5dfc071387676d6bf1
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-02-25 00:17:07 +0000

    testing stuff

--- Lab05/temp.py
+++ Lab05/temp.py
@@ -1,18 +1,29 @@
 # For general playing around
 
 # Andrews solution to lab 5.2 using pop
-import random
-queue = []
-numberOfNumbers=10
-rangeTo=100
+#import random
+#queue = []
+#numberOfNumbers=10
+#rangeTo=100
 
-for n in range (0, numberOfNumbers):
-    queue.append(random.randint(0,rangeTo))
-    
-print (f"queue is {queue}")
+#for n in range (0, numberOfNumbers):
+#    queue.append(random.randint(0,rangeTo))
+#    
+#print (f"queue is {queue}")
 
-while len(queue) != 0:
-    currentNumber = queue.pop(0)
-    print (f"current num is {currentNumber} and the queue is {queue}")
-print ("the queue is now empty")
+#while len(queue) != 0:
+#    currentNumber = queue.pop(0)
+#    print (f"current num is {currentNumber} and the queue is {queue}")
+#print ("the queue is now empty")
 
+student_file2 = {
+    "Name" : "", 
+    "Results" : [{
+        "Course" : "",
+        "Grade" : ""}
+    ]
+}
+student_file2["Name"] = 123
+student_file2["Results"."Course"]= "Irish"
+
+print (student_file2)
