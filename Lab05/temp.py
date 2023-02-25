7e1c67b251c4164ce95b3bbe51f08097dea131ba
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-02-25 23:26:10 +0000

    testing stuff

--- Lab05/temp.py
+++ Lab05/temp.py
@@ -16,14 +16,25 @@
 #    print (f"current num is {currentNumber} and the queue is {queue}")
 #print ("the queue is now empty")
 
+
 student_file2 = {
-    "Name" : "", 
+    "Name" : "Mary", 
     "Results" : [{
-        "Course" : "",
-        "Grade" : ""}
+        "Course" : "End",
+        "Grade" : "111"}
     ]
 }
-student_file2["Name"] = 123
-student_file2["Results"."Course"]= "Irish"
+
+student_file2["Name"] = "John"
+student_file2["Course"]= "Irish"
+student_file2["Grade"]= "77"
+newresult=100
+student_file2["Grade"][0]= newresult
+student_file2["Grade"]= "100"
+
+print (student_file2["Name"])
+print (student_file2["Results"])
+print (student_file2["Course"])
+print (student_file2["Grade"])
 
 print (student_file2)
