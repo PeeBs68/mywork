99230e9bc610510a7e47ae26ff6f5684dd050fc8
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-02-28 20:42:04 +0000

    Testing

--- Lab05/temp.py
+++ Lab05/temp.py
@@ -1,5 +1,9 @@
 # For general playing around
 
+
+dict = {'www': {'Course: 2': 'Grade: e', 'Course: 3': 'Grade: 5'}}
+print (dict)
+
 # Andrews solution to lab 5.2 using pop
 #import random
 #queue = []
@@ -17,24 +21,24 @@
 #print ("the queue is now empty")
 
 
-student_file2 = {
-    "Name" : "Mary", 
-    "Results" : [{
-        "Course" : "End",
-        "Grade" : "111"}
-    ]
-}
+#student_file2 = {
+#    "Name" : "Mary", 
+#    "Results" : [{
+#        "Course" : "End",
+#        "Grade" : "111"}
+#    ]
+#}
 
-student_file2["Name"] = "John"
-student_file2["Course"]= "Irish"
-student_file2["Grade"]= "77"
-newresult=100
-student_file2["Grade"][0]= newresult
-student_file2["Grade"]= "100"
+#student_file2["Name"] = "John"
+#student_file2["Course"]= "Irish"
+#student_file2["Grade"]= "77"
+#newresult=100
+#student_file2["Grade"][0]= newresult
+#student_file2["Grade"]= "100"
 
-print (student_file2["Name"])
-print (student_file2["Results"])
-print (student_file2["Course"])
-print (student_file2["Grade"])
+#print (student_file2["Name"])
+#print (student_file2["Results"])
+#print (student_file2["Course"])
+#print (student_file2["Grade"])
 
-print (student_file2)
+#print (student_file2)
