772116e26551239ec2bea84597e8bd550496bd74
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-03-02 17:04:23 +0000

    Lab6.3 In Progress

--- Lab06/lab6.3.py
+++ Lab06/lab6.3.py
@@ -1,19 +1,6 @@
-# Lab6.3
-# While Loop around the input
 # Lab06.3
 # Create the menu items function
 
-def menu():
-    print("Here are your options")
-    print("\t(a) Add new student")
-    print("\t(v) View Students")
-    print("\t(q) Quit")
-    item = (input("Type one letter (a/v/q):  "))
-
-    return item
-
-item = menu()
-print (f"You choose: {item}")
 def doAdd()
     return a1
     print("Adding")
