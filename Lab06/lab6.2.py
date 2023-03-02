c4fae0b52d19a61c7f362caa90a62591148f8910
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-03-02 17:01:19 +0000

    Lab6.2 Complete

--- Lab06/lab6.2.py
+++ Lab06/lab6.2.py
@@ -1,4 +1,6 @@
-# Lab06.2
+# Lab6.3
+# While Loop around the input
+# Lab06.3
 # Create the menu items function
 
 def menu():
@@ -7,6 +9,29 @@ def menu():
     print("\t(v) View Students")
     print("\t(q) Quit")
     item = (input("Type one letter (a/v/q):  "))
+
+    return item
+
+item = menu()
+print (f"You choose: {item}")
+def doAdd()
+    return a1
+    print("Adding")
+
+def doView()
+    print("Viewing")
+
+def menu():
+    print("Here are your options")
+    print("\t(a) Add new student")
+    print("\t(v) View Students")
+    print("\t(q) Quit")
+    item = (input("Type one letter (a/v/q):  "))
+    while item != "q":
+        if item == "a":
+            add = doAdd()
+        else
+            doView()
 
     return item
 
