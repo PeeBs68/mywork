e90a05285baa7e12cfdff1ab53208dc84dbd247a
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-03-06 23:23:45 +0000

    Lab6.5 working

--- Lab06/lab6.5.py
+++ Lab06/lab6.5.py
@@ -1,12 +1,23 @@
 #Lab6.5 which adds the entering the modules in readModules
 
-
-#create a list just for the names
+#This part from lab6.4
+#create a list
 students = []
 
-#temp - doesn't do anything for noe
 def readModules():
-    return []
+    #create another list
+    modules = []
+    module_name = input("Enter a module: ")
+    while len(module_name) > 0:
+        #create a dictionary for module which will have module and grade
+        module = {}
+        module["name"] = module_name
+        module["grade"] = int(input("Enter a grade: "))
+        #now append the dict into the list
+        modules.append(module)
+        module_name = input("Enter anther module: ")
+
+    return modules
 
 def doAdd(students):
     currentStudent = {}
