3befde226725e1d3b0e11f63cd004af05971eb8e
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-03-09 23:51:33 +0000

    Lab7.2c - nearly working right

--- Lab07/lab7.2c.py
+++ Lab07/lab7.2c.py
@@ -1,4 +1,7 @@
-FILENAME = "count.txt"
+#lab7.2c
+import os.path
+
+FILENAME = "counter9.txt"
 
 def update_counter(newcounter):
     with open(FILENAME, 'wt') as f:
@@ -7,11 +10,20 @@ def update_counter(newcounter):
 def read_number():
     with open(FILENAME, 'r') as f:
         counter = f.read()
-        counter = int(counter)
+        #counter = int(counter)
         return counter
 
+#check that the file exists - assume in the current directory
+if os.path.isfile(FILENAME):
+    print ("The file exists!!!")
+else:
+    print ("The file is not there - I'll create one")
+    f = open("counter9.txt", "x")
+    counter = 0
+    f.write(str(counter))
+
 counter = read_number()
-newcounter = counter+1
+newcounter = int(counter)+1
 
 update_counter(newcounter)
 
