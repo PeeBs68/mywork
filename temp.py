4d0fe3017efe1409bca5af3cf680d022cfebc5da
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-03-12 00:05:29 +0000

    testing stull for task07

--- temp.py
+++ temp.py
@@ -83,12 +83,19 @@ root = sqrt(num)
 print (f"Root of {num} = {root} using Newtons method")
 '''
 
+
+#Import the sys module
 import sys
+
+#get the filename from the command line
 FILENAME = sys.argv[1]
+
 #Note this will be an exact search - i.e. lowercase e only
 letter_to_find = "r"
 with open(FILENAME, 'r') as f:
         string1 = f.read()
+
+#count the number of e's (or whatever the variable letter_to_fine is set to)
 num_of_e = string1.count(letter_to_find)
 print (f"The number of {letter_to_find}'s in {FILENAME} is {num_of_e}")
 
