4ccee679eb3e0844841424d66abe3b9d5d5cafb8
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-03-15 23:30:55 +0000

    Project work area

--- Temp/Project_temp.py
+++ Temp/Project_temp.py
@@ -1,4 +1,28 @@
 #Temp file for working on stuff related to the project
 
-#Usefull Links
+#Project Plan: maybe like this???
+#Goals - It might help to suppose that your manager has asked you to investigate the data set, 
+#with a view to explaining it to your colleagues. Imagine that you are to give a presentation on 
+#the data set in a few weeks’ time, where you explain what investigating a data set entails and 
+#how Python can be used to do it
+#Objectives - 
+#Tasks - ???
+#Probably need to look at due dates etc also
+
+'''Usefull Links
 #https://www.geeksforgeeks.org/python-basics-of-pandas-using-iris-dataset/
+[1] Python Software Foundation. Welcome to python.org. https://www.python.org/. 
+[3] UC Irvine Machine Learning Repository. Iris data set. https://archive.ics.uci.edu/ml/datasets/iris
+UCI Machine Learning 
+Repository: Iris Data Set http://archive.ics.uci.edu/ml/datasets/Iris
+'''
+
+'''
+#Task suggestions
+1. Research the data set online and write a summary about it in your README.
+2. Download the data set and add it to your repository.
+3. Write a program called analysis.py that:
+1. Outputs a summary of each variable to a single text file, 2. Saves a histogram of each variable to png files, and
+3. Outputs a scatter plot of each pair of variables.
+4. Performs any other analysis you think is appropriate
+'''
