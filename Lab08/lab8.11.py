cff277cd9a871361e378d362fa66df48871b7da8
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-03-16 21:40:15 +0000

    lab 8.11 completed

--- Lab08/lab8.11.py
+++ Lab08/lab8.11.py
@@ -16,8 +16,17 @@ possible_counties = np.array(["Cork", "Wicklow", "Clare", "Limerick", "Kerry"])
 #Generates a random sample from a given 1-D array
 counties = np.random.choice(possible_counties, p=[0.1, 0.2, 0.3, 0.12, 0.28], size = (100))
 unique, counts = np.unique(counties, return_counts = True)
-plt.pie(counts, labels = unique)
+#Pie Chart
+#plt.pie(counts, labels = unique)
+
+print (counties)
+print (unique)
+print (counts)
+#Bar Chart
+plt.bar(unique, counts)
+
 plt.show()
 
 #Useful Link
 #https://www.geeksforgeeks.org/numpy-random-choice-in-python/
+#https://www.geeksforgeeks.org/bar-plot-in-matplotlib/
