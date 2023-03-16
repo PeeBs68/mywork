c2d89cdaf2f1a58325349bc8f01a98c9db30043f
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-03-16 14:51:51 +0000

    lab8.2 complete

---
+++ Lab08/lab8.2.py
@@ -0,0 +1,12 @@
+#Lab8.2
+
+import numpy as np
+
+salaries = []
+start_num = 20000
+end_num = 80000
+count_num = 10
+
+np.random.seed(1)
+numbers=np.random.randint(start_num,end_num,count_num)
+print ("Numbers", numbers)
