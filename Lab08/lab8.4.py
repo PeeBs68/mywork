dd9beea5c845d82faa85c32d4845738075cde077
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-03-16 15:00:38 +0000

    lab8.4 complete

--- Lab08/lab8.4.py
+++ Lab08/lab8.4.py
@@ -10,6 +10,7 @@ count_num = 10
 #np.random.seed(1)
 numbers=np.random.randint(start_num,end_num,count_num)
 new_numbers=numbers * 1.05
+#Could also have used floor() here to get rid of the decimal
 new_numbers = new_numbers.astype(int)
 print ("Numbers", numbers)
 print ("Numbers", new_numbers)
