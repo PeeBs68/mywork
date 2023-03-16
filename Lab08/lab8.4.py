e321df8626a42d13ff7e03d39771a5f9565e3692
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-03-16 15:00:01 +0000

    lab8.4 complete

---
+++ Lab08/lab8.4.py
@@ -0,0 +1,15 @@
+#Lab8.4
+
+import numpy as np
+
+salaries = []
+start_num = 20000
+end_num = 80000
+count_num = 10
+
+#np.random.seed(1)
+numbers=np.random.randint(start_num,end_num,count_num)
+new_numbers=numbers * 1.05
+new_numbers = new_numbers.astype(int)
+print ("Numbers", numbers)
+print ("Numbers", new_numbers)
