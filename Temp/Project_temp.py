255ea9689ffb4e94567243c23e3cfcb59557c87d
Phelim Barry <pbarry@pbarryYFVH3.vmware.com>
2023-03-16 01:21:16 +0000

    Project work area

--- Temp/Project_temp.py
+++ Temp/Project_temp.py
@@ -22,7 +22,8 @@ Repository: Iris Data Set http://archive.ics.uci.edu/ml/datasets/Iris
 1. Research the data set online and write a summary about it in your README.
 2. Download the data set and add it to your repository.
 3. Write a program called analysis.py that:
-1. Outputs a summary of each variable to a single text file, 2. Saves a histogram of each variable to png files, and
+1. Outputs a summary of each variable to a single text file, 
+2. Saves a histogram of each variable to png files, and
 3. Outputs a scatter plot of each pair of variables.
 4. Performs any other analysis you think is appropriate
 '''
