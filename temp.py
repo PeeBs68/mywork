e06d17a6e5203f798d48ad5e0a531e286869a1a2
Phelim Barry <phelimb@yahoo.com>
2023-10-29 23:53:03 +0000

    Testing loading iris data file

--- temp.py
+++ temp.py
@@ -1,7 +1,20 @@
-a = 1
+'''a = 1
 b = 2
 list = (a, b)
 print (list)
 a = 3
 list.append(a)
 print (list)
+'''
+
+
+#Import required modules
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# reading the CSV file and add column names
+# Information on how to add column names during the import taken from https://stackoverflow.com/questions/31645466/give-column-name-when-read-csv-file-pandas
+col_names =  ["Sepal Length", "Sepal Width", "Petal Length", "Petal Width", "Class"]
+iris_csv = pd.read_csv(data/'iris.data', sep= ",", names=col_names, header=None)
