f5fdc5dc8a47327e3efc7fa39cb42fbd6dc3dbc7
Phelim Barry <phelimb@yahoo.com>
2024-02-06 10:29:39 +0000

    Working on assignment1

--- WSAA/assignments_testing/currentweather_test.py
+++ WSAA/assignments_testing/currentweather_test.py
@@ -25,13 +25,14 @@ current_ws = data["current"]["wind_speed_10m"]
 print(f"The current wind speed at Kinsale Golf Club is {current_ws} kph.")
 '''
 
-url = "https://api.open-meteo.com/v1/forecast?latitude=53.82&longitude=-9.5&current=temperature_2m&current=wind_direction_10m"
+url = "https://api.open-meteo.com/v1/forecast?latitude=51.734507&longitude=-8.539811&current=temperature_2m&current=wind_direction_10m"
 response = requests.get(url)
 data = response.json()
 current_wdir = data["current"]["wind_direction_10m"]
 current_temp = data["current"]["temperature_2m"]
 
 # Just to add a bit more detail
+# https://uni.edu/storm/Wind%20Direction%20slide.pdf
 if current_wdir < 90:
     wdir = "North/Easterly"
 elif current_wdir < 180:
