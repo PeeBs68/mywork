e3419ef928d23af59a2ce50d9a1afed3100a7d18
Phelim Barry <phelimb@yahoo.com>
2024-02-19 20:52:38 +0000

    Working on Assignment3

--- WSAA/assignments_testing/assignment03-cso.py
+++ WSAA/assignments_testing/assignment03-cso.py
@@ -3,9 +3,13 @@ import json
 
 url = "https://ws.cso.ie/public/api.restful/PxStat.Data.Cube_API.ReadDataset/FIQ02/JSON-stat/2.0/en"
 
-response = requests.get(url)
-data = response.json()
-print(response) #200 is good
+def getdata():
+    response = requests.get(url)
+    data = response.json()
+    print(response) #200 is good
+    with open("cso.json", "wt") as fp:
+        print(json.dumps(data), file=fp)
 
-with open("cso.json", "wt") as fp:
-    print(json.dumps(data), file=fp)
+getdata()
+
+#N ote to self - use Shift+Alt+F to reformat JSON file
