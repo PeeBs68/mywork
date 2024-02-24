3b0d53cded5a21bbfebdb45c0f7ae573818d5693
Phelim Barry <phelimb@yahoo.com>
2024-02-24 20:52:13 +0000

    Working on Lab05 htmltopdf with keys

---
+++ WSAA/html2pdf.py
@@ -0,0 +1,26 @@
+import requests
+import urllib.parse
+#from config import apikeys as cfg
+
+targetUrl = "https://en.wikipedia.org"
+#targetUrl = "https://www.atu.ie/"
+
+#apikey = cfg["htmltopdfkey"]
+apikey = "Tsxnu5BVqAtLeB9DI6FPbeKSjwFeUqEWIRS7IlPMT04d96RrkVfuDUS7O0nPvavd"
+apiurl = 'https://api.html2pdf.app/v1/generate'
+
+params = {'html': targetUrl,'apiKey': apikey}
+parsedparams = urllib.parse.urlencode(params)
+
+requesturl = apiurl +"?" + parsedparams 
+print (requesturl)
+
+response = requests.get(requesturl)
+print(response.status_code)
+
+result = response.content
+with open ("document.pdf", "wb") as handler:
+    handler.write(result)
+
+
+
